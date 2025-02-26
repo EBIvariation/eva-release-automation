9d61a4579b7176128ed0ca0750290442083c4f22
April Shen <ashen@ebi.ac.uk>
2025-02-26 14:03:11 +0000

    update setup

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
+from os.path import join, abspath, dirname
 from setuptools import find_packages, setup
 
 
-def get_requires():
-    requires = []
-    with open("requirements.txt", "rt") as req_file:
-        for line in req_file:
-            requires.append(line.rstrip())
-    return requires
+base_dir = abspath(dirname(__file__))
+requirements_txt = join(base_dir, 'requirements.txt')
+requirements = [l.strip() for l in open(requirements_txt) if l and not l.startswith('#')]
 
-
-setup(name='run_release_in_embassy',
+setup(name='eva_release_automation',
       version='0.0.1',
       packages=find_packages(),
-      install_requires=get_requires(),
-      tests_require=get_requires(),
-      setup_requires=get_requires()
-)
+      package_data={'run_release_in_embassy': ['nextflow/*']},
+      install_requires=requirements,
+      license='Apache',
+      description='EBI EVA - RS Release processing tools',
+      url='https://github.com/EBIVariation/eva-release-automation',
+      python_requires='>=3.8',
+      author='The EVA team',
+      author_email='eva-dev@ebi.ac.uk',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9'
+      ]
+      )
