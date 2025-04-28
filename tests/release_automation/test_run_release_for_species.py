0122df0ed87870995311bb74c17ccf29daba9649
tcezard <tcezard@ebi.ac.uk>
2025-04-28 15:10:11 +0100

    reworked docker compose and add externally mounted volume

--- tests/release_automation/test_run_release_for_species.py
+++ tests/release_automation/test_run_release_for_species.py
@@ -1,14 +1,12 @@
 import os
+import shutil
 import time
 from unittest import TestCase
 
 import yaml
 from ebi_eva_common_pyutils.command_utils import run_command_with_output
-from ebi_eva_common_pyutils.config import cfg
 from ebi_eva_internal_pyutils.metadata_utils import get_metadata_connection_handle
 from ebi_eva_internal_pyutils.pg_utils import execute_query
-
-from release_automation.run_release_for_species import run_release_for_species
 
 resource_folder = os.path.join(os.path.dirname(__file__), 'resources')
 release_inventory = [
@@ -25,11 +23,10 @@ class TestRunReleaseForSpecies(TestCase):
     @classmethod
     def setUpClass(cls):
         os.chdir(cls.compose_dir)
-        command = 'docker compose up -d --wait'
+        command = 'docker compose up --build -d --wait'
         run_command_with_output('Start docker-compose in the background', command)
         # Wait for the postgres to be ready to accept command
-        # time.sleep(10)
-
+        time.sleep(10)
 
 
     @classmethod
@@ -38,40 +35,46 @@ class TestRunReleaseForSpecies(TestCase):
         command = 'docker compose down'
         run_command_with_output('Stop docker-compose running in the background', command)
 
+    def setUp(self):
+        self.output_directory =  os.path.join(resource_folder, 'release_output_dir', 'release_1')
+
+    def tearDown(self):
+        if os.path.isdir(self.output_directory):
+            shutil.rmtree(self.output_directory)
+
 
     def test_run_release_for_species(self):
-        config_xml = os.path.join(resource_folder, 'config_xml_file.xml')
-        config_yaml = os.path.join(resource_folder, 'release_config.yml')
-        nextflow_config = os.path.join(resource_folder, 'nextflow.config')
-        output_dir = os.path.join(resource_folder, 'release_output_dir')
-        nextflow_path = 'nextflow'
-        bcftools_path = 'bcftools'
-        profile = 'localhost'
-        inventory_table =  'eva_progress_tracker.clustering_release_tracker'
-        # cfg.content = {
-        #     'maven': {'environment': profile, 'settings_file': config_xml},
-        #     'release':{'release_output': output_dir, 'inventory_table': inventory_table},
-        #     'executable': {'nextflow': nextflow_path, 'bcftools': bcftools_path},
-        #     'jar':{'release_pipeline': '/Users/tcezard/PycharmProjects/eva-release-automation/tests/release_automation/resources/release_output_dir/release_1/oryza_sativa/GCA_000005425.2/eva-accession-release-0.6.47-20250422.150441-1-exec.jar'}
-        # }
-        fasta_file = os.path.join(resource_folder,'GCA_000005425.2.fa')
-        report_file = os.path.join(resource_folder,'GCA_000005425.2_assembly_report.txt')
-
-        with get_metadata_connection_handle(profile, config_xml) as metadata_connection_handle:
-            insert_query = (f"insert into {inventory_table} "
-                            f"(taxonomy, scientific_name, assembly_accession, release_version, sources, fasta_path, "
-                            f"report_path, should_be_released, num_rs_to_release, total_num_variants, release_folder_name) "
-                            f"values (4530, 'Oryza sativa', 'GCA_000005425.2', 1, 'EVA', '{fasta_file}', "
-                            f"'{report_file}', True, 1, 1, 'oryza_sativa')")
-            execute_query(metadata_connection_handle, insert_query)
-        # os.environ['RELEASE_CONFIG'] = config_yaml
-        # os.environ['RELEASE_NEXTFLOW_CONFIG'] = nextflow_config
+        add_data_to_docker()
+        expected_output = ('| taxonomy | assembly_accession | release_version | release_status |\n'
+                           '|     4530 |    GCA_000005425.2 |               1 |        Pending |\n')
+        expected_log = os.path.join(self.output_directory, 'oryza_sativa', 'GCA_000005425.2',
+                                    'release_active_rs_4530_GCA_000005425.2_1.log')
+        assert not os.path.isfile(expected_log)
         command = 'docker exec executor python3 -m  release_automation.run_release_for_species --list_status Pending'
         output = run_command_with_output('list all pending', command, True)
-        expected_output='''| taxonomy | assembly_accession | release_version | release_status |
-|     4530 |    GCA_000005425.2 |               1 |        Pending |
-'''
         assert expected_output == output
-        command = 'docker exec executor python3 -m  release_automation.run_release_for_species --taxonomy_id 4530 --assembly_accessions GCA_000005425.2 --release_version 1'
+        command = 'docker exec executor python3 -m release_automation.run_release_for_species --taxonomy_id 4530 --assembly_accessions GCA_000005425.2 --release_version 1'
         output = run_command_with_output('list all pending', command, True)
-        print(output)
+        # Check that the run has completed and that log files are present
+        assert os.path.isfile(expected_log)
+
+
+def add_data_to_docker():
+    config_xml = os.path.join(resource_folder, 'config_xml_file.xml')
+    profile = 'localhost'
+    inventory_table = 'eva_progress_tracker.clustering_release_tracker'
+    # Path inside the executor Docker image
+    fasta_file = '/opt/tests/release_automation/resources/GCA_000005425.2.fa'
+    report_file = '/opt/tests/release_automation/resources/GCA_000005425.2_assembly_report.txt'
+    with get_metadata_connection_handle(profile, config_xml) as metadata_connection_handle:
+        insert_query = (f"insert into {inventory_table} "
+                        f"(taxonomy, scientific_name, assembly_accession, release_version, sources, fasta_path, "
+                        f"report_path, should_be_released, num_rs_to_release, total_num_variants, release_folder_name) "
+                        f"values (4530, 'Oryza sativa', 'GCA_000005425.2', 1, 'EVA', '{fasta_file}', "
+                        f"'{report_file}', True, 1, 1, 'oryza_sativa')")
+        execute_query(metadata_connection_handle, insert_query)
+
+
+if __name__ == '__main__':
+    add_data_to_docker()
+
